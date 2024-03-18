02b9a77662a9c4a6724170e48ba7de6fb2789224
Abdul <127184231+101Viper777@users.noreply.github.com>
2024-03-18 15:06:17 +1100

    lab1 worked

--- Lab2/07-While_Loop.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-/*
- Control Flow - While
- A while loop performs a set of statements until a condition becomes false.
- */
-
-// You have three attemps to submit your assigment, write a while record the process you submit three times
--- Lab2/08-Pseudo_Code.playground/Contents.swift
+++
@@ -1,2 +0,0 @@
-// Now that you know some of the basic building blocks using Swift
-// It's time to try to Pseudo Code your Calculator Applicatoin!
